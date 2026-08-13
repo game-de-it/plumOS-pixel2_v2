c646a3d75ae2254f1ceb1adcde4799d3ae7f8031
game-de-it <game.de.it.0@gmail.com>
2026-08-13 18:13:55 +0900

    fix: use stable Mupen64Plus interpreter on Pixel2

--- tests/test-app-layer-scripts.sh
+++ tests/test-app-layer-scripts.sh
@@ -165,6 +165,10 @@ grep -q 'video_threaded = "true"' \
     "$ROOT_DIR/package/app-layer-pixel2/bin/plumos-retroarch-launch"
 grep -q 'video_refresh_rate = "%s"' \
     "$ROOT_DIR/package/app-layer-pixel2/bin/plumos-retroarch-launch"
+grep -q 'mupen64plus-cpucore cached_interpreter' \
+    "$ROOT_DIR/package/app-layer-pixel2/bin/plumos-retroarch-launch"
+grep -q 'core_options_path = "%s"' \
+    "$ROOT_DIR/package/app-layer-pixel2/bin/plumos-retroarch-launch"
 grep -q 'vrr_runloop_enable = "%s"' \
     "$ROOT_DIR/package/app-layer-pixel2/bin/plumos-retroarch-launch"
 grep -q '#define VOLUME_PROBE_INTERVAL 0' \
